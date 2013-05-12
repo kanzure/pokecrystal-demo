8f0b4241eb690ef3ef78a6d8d24cbf3dc6444440
Bryan Bishop <kanzure@gmail.com>
2013-05-12 17:46:12 -0500

    walk straight into the forest

--- maps/KrissHouse2F.asm
+++ maps/KrissHouse2F.asm
@@ -142,7 +142,8 @@ KrissHouse2F_MapEventHeader: ; 0x7ac99
 	db 1
 
 ;	warp_def 0, 7, 1, GROUP_WATERCHUTEWAY, MAP_WATERCHUTEWAY
-	warp_def 0, 7, 1, GROUP_THIRDCAVE, MAP_THIRDCAVE
+;	warp_def 0, 7, 1, GROUP_THIRDCAVE, MAP_THIRDCAVE
+	warp_def 0, 7, 1, GROUP_SPIRALFOREST, MAP_SPIRALFOREST
 ;	warp_def $0, $7, 3, GROUP_KRISS_HOUSE_1F, MAP_KRISS_HOUSE_1F
 
 	; xy triggers
